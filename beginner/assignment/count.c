9ba09018643c3c01b556f7862c79bddba6a9c0cc
RITHIKSANJEEV97 <34641065+RITHIKSANJEEV97@users.noreply.github.com>
2017-12-19 12:29:58 +0530

    Create count.c

---
+++ beginner/assignment/count.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+static int count=11;//global
+declartion
+
+
+int main(void) {
+
+
+int count=5;//local
+declaration
+
+
+while(count>0)
+
+
+{
+
+
+printf("%d\n",count);
+
+
+count--;
+
+
+}
+
+
+}
+
+
